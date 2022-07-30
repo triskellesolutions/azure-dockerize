c3f48b98718e74250836fb2842b7f65e82a2a559
John Babb <john.babb@triskelle.solutions>
2022-07-30 06:37:49 -0400

    creating local file  install location

--- ubuntu-docker-superset/install.sh
+++ ubuntu-docker-superset/install.sh
@@ -17,6 +17,7 @@ IFS=$'\n\t'
 supersetAdminUser=$1
 supersetAdminUserPassword=$2
 supersetDomainName=$3
+remoteFileUrl=$4
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -27,6 +28,7 @@ sudo dpkg-reconfigure -plow unattended-upgrades
 echo "---------------------------------------------------------------------------"
 echo "- Install certbot"
 echo "---------------------------------------------------------------------------"
+sudo apt install snapd
 sudo snap install core; sudo snap refresh core;
 sudo apt-get remove certbot
 sudo snap install --classic certbot
@@ -34,22 +36,40 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 sudo certbot certonly --standalone --agree-tos --email certbot@triskelle.solutions -d $supersetDomainName
 
-# sudo docker run -e "SUPERSET_OAUTH_KEY=" \
-#    -e "SUPERSET_OAUTH_SECRET=" \
-#    -e "SUPERSET_OAUTH_WHITELIST=" \
-#     -e "SUPERSET_HOME=/home/superset" \
-#    --restart always \
-#    --name superset \
-#    -p 80:8080 \
-#    -d apache/superset
+installScript() {
+    local fileName="$1"
+    local filePath="$2"
+    local remoteUrl="$3"
+    sudo touch $filePath$fileName
+    sudo chmod 777  $filePath$fileName
+    sudo curl -sl "${remoteUrl}/${fileName}" > $filePath/$fileName
+    sudo chown root:root $filePath$fileName
+    sudo chmod 600  $filePath$fileName
+    sudo chmod +x  $filePath$fileName
+}
 
-# sudo docker exec -it superset superset fab create-admin \
-#    --username $supersetAdminUser \
-#    --firstname Superset \
-#    --lastname Admin \
-#    --email superset.admin@triskelle.solutions \
-#    --password $supersetAdminUserPassword
+install Script superset_config.py $PWD "$remoteFileUrl"
 
-# sudo docker exec -it superset superset db upgrade
-# sudo docker exec -it superset superset load_examples
-# sudo docker exec -it superset superset init
+# -e "SUPERSET_OAUTH_KEY=" \
+# -e "SUPERSET_OAUTH_SECRET=" \
+# -e "SUPERSET_OAUTH_WHITELIST=" \
+# -e "SUPERSET_HOME=/home/superset" \
+
+sudo docker run \
+   --restart always \
+   --name superset \
+   -p 80:8088 \
+   -p 443:8088 \
+   -v ./superset_config.py:/etc/superset/superset_config.py
+   -d apache/superset
+
+sudo docker exec -it superset superset fab create-admin \
+   --username $supersetAdminUser \
+   --firstname Superset \
+   --lastname Admin \
+   --email superset.admin@triskelle.solutions \
+   --password $supersetAdminUserPassword 
+
+sudo docker exec -it superset superset db upgrade
+sudo docker exec -it superset superset load_examples
+sudo docker exec -it superset superset init
